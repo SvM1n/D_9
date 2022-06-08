fa68c82afbe76d0b28f54226a143959425f45790
SvM1n <101259356+SvM1n@users.noreply.github.com>
2022-06-08 23:24:17 +0300

    Add files via upload

---
+++ Z9.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Dom9.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Property* ar[7];
+	for (int i = 0; i < 7; i++)
+	{
+		if (i < 3)
+		{
+			ar[i] = new Apartment(rand() % 10000 + 1000);
+		}
+		else if (i < 5) {
+			ar[i] = new Car(rand() % 10000 + 1000);
+		}
+		else {
+			ar[i] = new CountryHouse(rand() % 10000 + 1000);
+		}
+
+	}
+	for (int i = 0; i < 7; i++) {
+		ar[i]->nal();
+		delete [] ar[i];
+	}
+
+}
